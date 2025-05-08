b4ad45636e6a32206a13aa21c5883fef6ab21f85
Tushar-Mehra1707 <tusharmehra4321@gmail.com>
2025-05-08 10:17:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+// script.js
 
-  //your code here
-	let result = "";
+// The Roman numerals chart with corresponding values
+const romanSymbols = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+];
 
-  for (let [symbol, value] of romanNumerals) {
-    while (x >= value) {
-      result += symbol;
-      x -= value;
+// Function to convert a number to Roman numeral
+function toRoman(num) {
+    if (num < 1 || num > 100000) {
+        return "Number out of range (1-100000)";
     }
-  }
 
-  return result;
+    let roman = '';
+    // Iterate over the romanSymbols array
+    for (let i = 0; i < romanSymbols.length; i++) {
+        while (num >= romanSymbols[i][1]) {
+            roman += romanSymbols[i][0];
+            num -= romanSymbols[i][1];
+        }
+    }
+
+    return roman;
 }
 
-
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+// Adding event listener to the button to trigger conversion
+document.getElementById("convertButton").addEventListener("click", function () {
+    const num = parseInt(document.getElementById("numberInput").value);
+    const roman = toRoman(num);
+    document.getElementById("romanResult").innerText = roman;
+});
